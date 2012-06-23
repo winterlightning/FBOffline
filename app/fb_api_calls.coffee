29dc4878cd797cbcfb075d57cdc9556f6926b5ea
winterlightning <raycornellwang@gmail.com>
2012-06-23 17:06:12 -0400

    working api call structure

--- app/fb_api_calls.coffee
+++ app/fb_api_calls.coffee
@@ -4,5 +4,14 @@ window.fb_match =
     url: "/me"
     method: "GET"
 
-window.fb_call = () ->
+window.fb_base = "https://graph.facebook.com"
+
+window.fb_call = ( obj, cb ) ->
+  params = {}
+  params.method = obj.method
+  params.access_token = localStorage.accessToken
   
+  $.getJSON("https://graph.facebook.com"+obj.url, params, (res)-> 
+    console.log(res) 
+    cb(res) if cb?
+  )
--- app/fb_api_calls.js
+++ app/fb_api_calls.js
@@ -5,5 +5,17 @@
       method: "GET"
     }
   };
-  window.fb_call = function() {};
+  window.fb_base = "https://graph.facebook.com";
+  window.fb_call = function(obj, cb) {
+    var params;
+    params = {};
+    params.method = obj.method;
+    params.access_token = localStorage.accessToken;
+    return $.getJSON("https://graph.facebook.com" + obj.url, params, function(res) {
+      console.log(res);
+      if (cb != null) {
+        return cb(res);
+      }
+    });
+  };
 }).call(this);
