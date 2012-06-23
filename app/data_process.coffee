d45bb97f84f589700c50ebbaef77d80d5740c761
winterlightning <raycornellwang@gmail.com>
2012-06-23 19:03:32 -0400

    draft for suck down feed

--- app/data_process.coffee
+++ app/data_process.coffee
@@ -1,5 +1,19 @@
 #take all the fb json and do something with it
 
 window.suck_down_feed = (json)->
-  for x in json
-    console.log(x)
+  for x in json.data
+    data = {}
+    
+    for field in Feed.attributes
+      if field.indexOf("_") is -1
+        data[field] = x[field] if x[field]?
+      else
+        field_a = field.split("_")[0]
+        field_b = field.split("_")[1]
+        
+        one = x[field_a]
+        data[field] = one[field_b]
+
+    console.log(data)
+
+    Feed.create(data)
---
+++ app/data_process.js
@@ -0,0 +1,28 @@
+(function() {
+  window.suck_down_feed = function(json) {
+    var data, field, field_a, field_b, one, x, _i, _j, _len, _len2, _ref, _ref2, _results;
+    _ref = json.data;
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      x = _ref[_i];
+      data = {};
+      _ref2 = Feed.attributes;
+      for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+        field = _ref2[_j];
+        if (field.indexOf("_") === -1) {
+          if (x[field] != null) {
+            data[field] = x[field];
+          }
+        } else {
+          field_a = field.split("_")[0];
+          field_b = field.split("_")[1];
+          one = x[field_a];
+          data[field] = one[field_b];
+        }
+      }
+      console.log(data);
+      _results.push(Feed.create(data));
+    }
+    return _results;
+  };
+}).call(this);
