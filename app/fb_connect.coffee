bfddb58f88391ece5adb813412ec184c70b9171a
winterlightning <raycornellwang@gmail.com>
2012-06-23 15:53:05 -0400

    standard script for fb chrome auth hack

---
+++ app/fb_connect.coffee
@@ -0,0 +1,16 @@
+window.onFacebookLogin = ->
+  unless localStorage.accessToken
+    chrome.tabs.getAllInWindow null, (tabs) ->
+      i = 0
+
+      while i < tabs.length
+        if tabs[i].url.indexOf(successURL) is 0
+          params = tabs[i].url.split("#")[1]
+          console.log params
+          localStorage.accessToken = params
+          chrome.tabs.onUpdated.removeListener onFacebookLogin
+          return
+        i++
+        
+window.successURL = "https://www.facebook.com/connect/login_success.html"
+chrome.tabs.onUpdated.addListener onFacebookLogin
---
+++ app/fb_connect.js
@@ -0,0 +1,24 @@
+(function() {
+  window.onFacebookLogin = function() {
+    if (!localStorage.accessToken) {
+      return chrome.tabs.getAllInWindow(null, function(tabs) {
+        var i, params, _results;
+        i = 0;
+        _results = [];
+        while (i < tabs.length) {
+          if (tabs[i].url.indexOf(successURL) === 0) {
+            params = tabs[i].url.split("#")[1];
+            console.log(params);
+            localStorage.accessToken = params;
+            chrome.tabs.onUpdated.removeListener(onFacebookLogin);
+            return;
+          }
+          _results.push(i++);
+        }
+        return _results;
+      });
+    }
+  };
+  window.successURL = "https://www.facebook.com/connect/login_success.html";
+  chrome.tabs.onUpdated.addListener(onFacebookLogin);
+}).call(this);
